ca390126426176ac9ba78cd792e27bf07a12a197
prudhviraj30505 <vtu30505@veltech.edu.in>
2026-09-03 21:55:46 +0530

    Add solution for querying elements in multiple lists
    Implement a solution to handle multiple lists and queries with error handling for out-of-bounds access.

---
+++ week 3/session 13 task 2.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();   // number of lists
+        ArrayList<ArrayList<Integer>> lists = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int d = sc.nextInt();   // number of elements in this list
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int j = 0; j < d; j++) {
+                list.add(sc.nextInt());
+            }
+            lists.add(list);
+        }
+        int q = sc.nextInt(); 
+        for (int i = 0; i < q; i++) {
+            int x = sc.nextInt();   // list index
+            int y = sc.nextInt();   // element index
+            try {
+                System.out.println(lists.get(x - 1).get(y - 1));
+            } catch (Exception e) {
+                System.out.println("ERROR!");
+            }
+        }
+        sc.close();
+    }
+}
+
+OUTPUT:
+
+Compiler Message
+Success
+Input (stdin)
+5
+5 41 77 74 22 44
+1 12
+4 37 34 36 52
+0
+3 20 22 33
+5
+1 3
+3 4
+3 1
+4 3
+5 5
+Expected Output
+74
+52
+37
+ERROR!
+ERROR!
