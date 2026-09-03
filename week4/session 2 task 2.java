eb6753c1eefd163fd4049fcf30c60b766388ffaa
prudhviraj30505 <vtu30505@veltech.edu.in>
2026-09-03 22:23:14 +0530

    Create session 2 task 2.java

---
+++ week4/session 2 task 2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }      
+        return false;
+    }
+}
+
+OUTPUT:
+
+Input
+nums =
+[1,2,3,1]
+Output
+true
+Expected
+true
