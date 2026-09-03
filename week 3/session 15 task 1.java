304b4d63286575a776f0a315845e1df710bcded8
prudhviraj30505 <vtu30505@veltech.edu.in>
2026-09-03 21:57:57 +0530

    Add frequencySort method to sort characters
    Implement frequency sort for characters in a string.

---
+++ week 3/session 15 task 1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> freqMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+        List<Character> chars = new ArrayList<>(freqMap.keySet());
+        chars.sort((a, b) -> freqMap.get(b) - freqMap.get(a));
+        StringBuilder sb = new StringBuilder();
+        for (char c : chars) {
+            int count = freqMap.get(c);
+            for (int i = 0; i < count; i++) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
+
+OUTPUT:
+
+Input
+s =
+"tree"
+Output
+"eert"
+Expected
+"eert"
