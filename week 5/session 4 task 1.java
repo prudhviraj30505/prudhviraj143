af8a0fa45451d3c0b73326c18ff1d84a259bdbc6
prudhviraj30505 <vtu30505@veltech.edu.in>
2026-09-03 23:43:19 +0530

    Create session 4 task 1.java

---
+++ week 5/session 4 task 1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> seen = new HashSet<>();
+        int left = 0, maxLen = 0;
+        for (int right = 0; right < s.length(); right++) {
+            while (seen.contains(s.charAt(right))) {
+                seen.remove(s.charAt(left));
+                left++;
+            }
+            seen.add(s.charAt(right));
+            maxLen = Math.max(maxLen, right - left + 1);
+        }    
+        return maxLen;
+    }
+}
+
+OUTPUT:
+
+Input
+s =
+"abcabcbb"
+Output
+3
+Expected
+3
