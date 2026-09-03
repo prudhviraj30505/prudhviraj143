78a8e25aacbfdf710f74bf62d27901e1bff12e39
prudhviraj30505 <vtu30505@veltech.edu.in>
2026-09-03 21:31:28 +0530

    Add topKFrequent method to find frequent elements
    Implemented a solution to find the top K frequent elements in an array.

---
+++ week2/session 9 task 2.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        List<Integer>[] buckets = new List[nums.length + 1];
+        for (int key : countMap.keySet()) {
+            int freq = countMap.get(key);
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(key);
+        }
+        int[] result = new int[k];
+        int index = 0;    
+        for (int i = buckets.length - 1; i >= 0 && index < k; i--) {
+            if (buckets[i] != null) {
+                for (int num : buckets[i]) {
+                    result[index++] = num;
+                    if (index == k) {
+                        return result;
+                    }
+                }
+            }
+        }    
+        return result;
+    }
+}
+
+OUTPUT:
+
+Input
+nums =
+[1,1,1,2,2,3]
+k =
+2
+Output
+[1,2]
+Expected
+[1,2]
