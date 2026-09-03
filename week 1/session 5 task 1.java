07715e910131f67d3c6715410111dba23e2c719f
prudhviraj30505 <vtu30505@veltech.edu.in>
2026-09-03 20:56:11 +0530

    Implement sortedSquares method in Solution class

---
+++ week 1/session 5 task 1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            int square;
+            if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+                square = nums[right];
+                right--;
+            } else {
+                square = nums[left];
+                left++;
+            }
+            result[i] = square * square;
+        }
+        return result;
+    }
+}
+
+OUTPUT:
+
+Input
+nums =
+[-4,-1,0,3,10]
+Output
+[0,1,9,16,100]
+Expected
+[0,1,9,16,100]
